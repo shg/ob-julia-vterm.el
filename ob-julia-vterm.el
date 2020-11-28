d527584245fd7ff8a9606f33f456db5d45e44457
Shigeaki Nishina <shgaki@gmail.com>
2020-11-28 17:37:41 +0900

    Implement session

--- ob-julia-vterm.el
+++ ob-julia-vterm.el
@@ -41,58 +41,48 @@
 (require 'org-macs)
 (require 'julia-vterm)
 
-(defconst org-babel-julia-vterm-evaluate-statement
-  "using Suppressor; output = @capture_out begin; include(\"%s\"); end; open(\"%s\", \"w\") do file; write(file, output); end\n")
+(add-to-list 'org-src-lang-modes '("julia-vterm" . "julia"))
 
-(defconst org-babel-julia-vterm-evaluate-statement-to-value
-  "output = begin; include(\"%s\"); end; open(\"%s\", \"w\") do file; print(file, output); end\n")
+(defun org-babel-julia-vterm--make-src (result-type with-session body)
+  "RESULT-TYPE WITH-SESSION BODY."
+  (concat
+   (if (eq result-type 'output)
+       "_julia_vterm_output = @capture_out " "_julia_vterm_output = ")
+   (if with-session
+       "begin\n" "let\n")
+   body
+   "\nend\n"))
+
+(defun org-babel-julia-vterm--make-str-to-run (src-file out-file)
+  "SRC-FILE OUT-FILE."
+  (format "using Suppressor; include(\"%s\");  open(\"%s\", \"w\") do file; print(file, _julia_vterm_output); end\n" src-file out-file))
 
 (unless (fboundp 'org-babel-execute:julia)
   (defalias 'org-babel-execute:julia 'org-babel-execute:julia-vterm))
 
 (defun org-babel-execute:julia-vterm (body params)
   "Execute a block of Julia code with Babel.
-This function is called by `org-babel-execute-src-block'."
+This function is called by `org-babel-execute-src-block'.
+BODY is the contents and PARAMS are header arguments of the code block."
   (let* ((session (cdr (assq :session params)))
 	 (result-params (cdr (assq :result-params params)))
 	 (result-type (cdr (assq :result-type params)))
 	 (full-body (org-babel-expand-body:generic body params)))
-    (message "body = %s" body)
-    (message "full-body = %s" full-body)
-    (org-babel-julia-vterm-evaluate session full-body result-type result-params)
-    ))
+    (org-babel-julia-vterm-evaluate session full-body result-type result-params)))
 
 (defun org-babel-julia-vterm-evaluate (session body result-type result-params)
-  ;; session is ignored for now.
-  (let ((repl (julia-vterm-repl-buffer))))
-  (message "session = %s" session)
-  (message "result-params = %s" result-params)
-  (message "result-type = %s" result-type)
-  (message "body = %s" body)
-  (pcase result-type
-    ('output 
-     (let ((src-file (org-babel-temp-file "julia-vterm-src-"))
-	   (out-file (org-babel-temp-file "julia-vterm-out-")))
-       (with-temp-file src-file (insert body))
-       (julia-vterm-paste-string
-	(format org-babel-julia-vterm-evaluate-statement src-file out-file))
-       (while (= 0 (file-attribute-size (file-attributes out-file)))
-	 (sit-for 0.1))
-       (with-temp-buffer
-	 (insert-file-contents out-file)
-	 (buffer-string)))
-     )
-    ('value
-     (let ((src-file (org-babel-temp-file "julia-vterm-src-"))
-	   (out-file (org-babel-temp-file "julia-vterm-out-")))
-       (with-temp-file src-file (insert body))
-       (julia-vterm-paste-string
-	(format org-babel-julia-vterm-evaluate-statement-to-value src-file out-file))
-       (while (= 0 (file-attribute-size (file-attributes out-file)))
-	 (sit-for 0.1))
-       (with-temp-buffer
-	 (insert-file-contents out-file)
-	 (buffer-string))))))
+  "Evaluate BODY as Julia code in a julia-vterm buffer specified with SESSION."
+  (let ((src-file (org-babel-temp-file "julia-vterm-src-"))
+	(out-file (org-babel-temp-file "julia-vterm-out-"))
+	(src (org-babel-julia-vterm--make-src result-type (not (string= session "none")) body)))
+    (with-temp-file src-file (insert src))
+    (julia-vterm-paste-string
+     (org-babel-julia-vterm--make-str-to-run src-file out-file)
+     (if (string= session "none") nil session))
+    (while (= 0 (file-attribute-size (file-attributes out-file)))
+      (sit-for 0.1))
+    (let ((result (with-temp-buffer (insert-file-contents out-file) (buffer-string))))
+      result)))
 
 (provide 'ob-julia-vterm)
 
