22f8825185231654397ae31e2a586ec39f98ab6f
shg <55463+shg@users.noreply.github.com>
2023-08-16 08:32:30 +0900

    Bump version to 0.3

--- ob-julia-vterm.el
+++ ob-julia-vterm.el
@@ -7,7 +7,7 @@
 ;; Created: October 31, 2020
 ;; URL: https://github.com/shg/ob-julia-vterm.el
 ;; Package-Requires: ((emacs "26.1") (julia-vterm "0.16") (queue "0.2"))
-;; Version: 0.2j
+;; Version: 0.3
 ;; Keywords: julia, org, outlines, literate programming, reproducible research
 
 ;; This file is not part of GNU Emacs.
